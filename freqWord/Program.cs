dff889de53112ba9cc1d92384afaf932733e0099
Briangosnel <109523312+Briangosnel@users.noreply.github.com>
2022-07-18 21:33:36 +0300

    Добавьте файлы проекта.

---
+++ freqWord/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace freqWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "";
+            Console.WriteLine("Загрузить текст для анализа? (y/n)");
+            string yn = Console.ReadLine().Trim().ToLower();
+            if (yn == "y")
+            {
+                Console.Write("Укажите путь к файлу:");
+                string filename = Console.ReadLine();
+                if (System.IO.File.Exists(filename) == false)
+                {
+                    Console.WriteLine("Вы ввели несуществующий путь к файлу. ");
+                    Console.WriteLine("Введите текст для анализа: ");
+                    text = Console.ReadLine().ToLower();
+                }
+                else
+                {
+                    text = System.IO.File.ReadAllText(filename);
+                }
+            }
+            else if (yn == "n")
+            {
+                Console.WriteLine("Введите текст для анализа: ");
+                text = Console.ReadLine().ToLower();
+            }
+            else
+            {
+                Console.WriteLine("Что-то вы не то ввели.");
+            }
+            string[] words = text.Split(new char[] { '-', '.', '?', '!', ')', '(', ',', ':', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length > 0) { Console.WriteLine("Общее количество слов :" + words.Length); }
+            var result = words.GroupBy(x => x)
+                .Select(x => new { Word = x.Key, Repeating = x.Count()});
+                foreach (var item in result)
+                {
+                    Console.WriteLine("Слово: {0}\tКоличество повторов: {1}", item.Word, item.Repeating);
+                }
+        }
+    }
+}
