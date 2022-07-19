ee90b6a8bfe7d58c59739cb5f5ca040e60b0178a
Briangosnel <109523312+Briangosnel@users.noreply.github.com>
2022-07-19 15:48:30 +0300

    Создание проекта тестирования

--- TestFreqWord/Program.cs
+++ TestFreqWord/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace TestFreqWord
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
---
+++ TestFreqWord/wordsTest.cs
@@ -0,0 +1,14 @@
+using freqWord;
+
+namespace TestFreqWord;
+
+[TestClass]
+public class wordsTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        freqWord.Program.text = "test, test. test";
+
+    }
+}
