a94beb3ac7131321be6aa0148349eca185499b60
Briangosnel <109523312+Briangosnel@users.noreply.github.com>
2022-07-19 14:48:35 +0300

    Разделение кода на 2 метода

--- freqWord/Program.cs
+++ freqWord/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace freqWord
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static string? text ;
+        public static void Main(string[] args)
         {
-            string text = "";
             Console.WriteLine("Загрузить текст для анализа? (y/n)");
             string yn = Console.ReadLine().Trim().ToLower();
             if (yn == "y")
@@ -16,29 +16,35 @@
                     Console.WriteLine("Вы ввели несуществующий путь к файлу. ");
                     Console.WriteLine("Введите текст для анализа: ");
                     text = Console.ReadLine().ToLower();
+                    words();
                 }
                 else
                 {
                     text = System.IO.File.ReadAllText(filename);
+                    words();
                 }
             }
             else if (yn == "n")
             {
                 Console.WriteLine("Введите текст для анализа: ");
                 text = Console.ReadLine().ToLower();
+                words();
             }
             else
             {
                 Console.WriteLine("Что-то вы не то ввели.");
             }
+        }
+            public static void words()
+            { 
             string[] words = text.Split(new char[] { '-', '.', '?', '!', ')', '(', ',', ':', ' ' }, StringSplitOptions.RemoveEmptyEntries);
             if (words.Length > 0) { Console.WriteLine("Общее количество слов :" + words.Length); }
             var result = words.GroupBy(x => x) // Группировка слов-элементов массива с помощью Языка Интегрированных Запросов
-                .Select(x => new { Word = x.Key, Repeating = x.Count()}); // Представление результатов в виде перечисления анонимного типа.
+                .Select(x => new { Word = x.Key, Repeating = x.Count() }); // Представление результатов в виде перечисления анонимного типа.
             foreach (var item in result)
-                {
-                    Console.WriteLine("Слово: {0}\tКоличество повторов: {1}", item.Word, item.Repeating);
-                }
+            {
+                Console.WriteLine("Слово: {0}\tКоличество повторов: {1}", item.Word, item.Repeating);
+            }
+        }
         }
     }
-}
